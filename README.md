3428e38acd21345661abd5cd7591a66a3d3d7c90
joe <jrspam-hp@web-dreamer.de>
2018-05-13 20:27:17 +0200

    init2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Scan2Pi2OCR
 This Project aims to have a RASPI as Saned-Scan-Engine and send the scanned documents to a second more powerfull computer for OCR and potentiall put to cloud.
+
+```console
+sudo apt-get install imagemagick bc exactimage pdftk \
+   tesseract-ocr tesseract-ocr-deu tesseract-ocr-eng
+```
+
+For use of Buttons i use:
+https://github.com/abusenius/insaned
+
+
--- raspi/scan.sh
+++ raspi/scan.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Thanks to Andreas Gohr (http://www.splitbrain.org/) for the initial work
 # https://github.com/splitbrain/paper-backup/
+# add output to shell if run directly as well put it to syslog
 exec 1> >(logger -s -t $(basename $0)) 2>&1
+
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 OUT_DIR=/media/scanner
 TMP_DIR=`mktemp -d`
 FILE_NAME=scan_`date +%Y%m%d-%H%M%S`
-LANGUAGE="deu"
+
 echo 'scanning...'
 scanimage --resolution 300 \
 --batch="$TMP_DIR/scan_%03d.pnm" \
@@ -13,6 +18,10 @@ scanimage --resolution 300 \
 --mode Gray \
 --source 'ADF Duplex'
 echo "Output saved in $TMP_DIR/scan*.pnm"
+
+#problem: scanner runs only with root... insaned.service
 chown -R joe:scanner $TMP_DIR
-runuser -l joe -c "/home/joe/ocrit.sh $OUT_DIR $TMP_DIR" & 
+# create new process and free ressources to enable a new scan.
+runuser -l joe -c "$DIR/ocrit.sh $OUT_DIR $TMP_DIR" & 
+
 echo "send all to OCR"
