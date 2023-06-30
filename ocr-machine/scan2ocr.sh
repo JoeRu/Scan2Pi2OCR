aeac0f894500f99fe86daf7b622cc058723c0b66
johannes.rumpf@gmail.com <johannes.rumpf@gmail.com>
2023-06-30 22:27:19 +0200

    updates to use nixos

--- ocr-machine/scan2ocr.sh
+++ ocr-machine/scan2ocr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!bash
 # https://github.com/JoeRu/Scan2Pi2OCR
 # Licensed under GPLv3 // Author: Johannes Rumpf - https://web-dreamer.de
 # twitter: @jay_ar
@@ -10,32 +10,37 @@ exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 #reading config
 source ./scan2ocr.config.sh
+#echo $PATH
 
 #echo $TMP_DIR
-echo "Start to OCR"
+echo "Start to OCR - $TMP_DIR"
 cd $TMP_DIR
 
 
 #https://superuser.com/questions/343385/detecting-blank-image-files
 # removing blank pages by checking black vs. white pixel proportions in histogram. 
 mkdir -p "blanks"
-for i in scan_*.pnm; do
+for i in scan_*.pnm.tif; do
     echo "${i}"
     if [[ -e $(dirname "$i")/.$(basename "$i") ]]; then
         echo "   protected."
         continue
     fi
-
+#    echo "convert "${i}" -threshold 50% -format %c histogram:info:-)"
     histogram=$(convert "${i}" -threshold 50% -format %c histogram:info:-)
-    #echo $histogram
-    white=$(echo "${histogram}" | grep "white" | cut -d: -f1)
-    black=$(echo "${histogram}" | grep "black" | cut -d: -f1)
+#    echo "histogramm: $histogram"
+    white=$(echo "${histogram}" | grep "(255,255,255)" | cut -d: -f1)
+#   echo "(echo "${histogram}" | grep "white" | cut -d: -f1"
+    black=$(echo "${histogram}" | grep "(0,0,0)" | cut -d: -f1)
+#   echo "echo "${histogram}" | grep "black" | cut -d: -f1"
+#    echo "white: $white black: $black"
     if [[ -z "$black" ]]; then
         black=0
     fi
 # black below 0.05 Percent of Pixels allover --> than blank
     blank=$(echo "scale=4; ${black}/${white} < 0.005" | bc)
-    #echo $white $black $blank
+#    echo "(echo \"scale=4; ${black}/${white} < 0.005\" | bc)"
+#    echo "xX: $white schwarz: $black blank: $blank"
     if [ "${blank}" -eq "1" ]; then
         echo "${i} seems to be blank - removing it..."
         mv "${i}" "blanks/${i}"
@@ -52,7 +57,7 @@ done
 
 # apply Contrast-Cleaning
 echo 'cleaning pages...'
-for i in scan_*.pnm; do
+for i in scan_*.pnm.tif; do
     echo "${i}"
     convert "${i}" -brightness-contrast 1x40% "${i}"
 done
@@ -67,17 +72,22 @@ done
 #done
 
 # convert to tif and compress a little with lzw
-echo 'cleaning pages...'
-for i in scan_*.pnm; do
-    echo "${i}"
-    convert "${i}" -compress jpeg "${i}.tif"
-done
+
+#echo 'compressing pages...'
+#for i in scan_*.pnm; do
+#    echo "${i}"
+#    /usr/bin/convert "${i}" -compress jpeg "${i}.tif"
+#done
 
 rm scan_list.txt
 # do OCR
 echo 'doing OCR...'
 for i in scan_*.pnm.tif; do
     echo "${i}" >> scan_list.txt
+# using tesseract
+#    tesseract "$i" "$i" -l $LANGUAGE hocr
+#    hocr2pdf -i "$i" -s -o "$i.pdf" < "$i.hocr"
+
 # using abby-cloud
 # https://ocrsdk.com/documentation/quick-start-guide/python-ocr-sdk/
 #    python $PATH_TO_ABBY_PROCESS_PY/process.py -l German -pdf "${i}" "${i}".pdf 
@@ -88,11 +98,11 @@ done
 # @todo adapt path in container and run:
 # sudo docker exec -t t4re /bin/bash -c "cd ./$1/; tesseract scan_list.txt $FILE_NAME -l deu --psm 1 --oem 2 txt pdf hocr"
 
-tesseract scan_list.txt $FILE_NAME -l deu+eng+frk --psm 1 --oem 2 txt pdf hocr
+tesseract scan_list.txt $FILE_NAME --dpi 300 --oem 1 -l deu+eng+frk --psm 1 txt pdf hocr
 
 cp $FILE_NAME.pdf $OUT_DIR/
 cd $WATCH_SCANS
-sudo chown -R $USER:$USER $WATCH_SCANS
+#sudo chown -R $USER:$USER $WATCH_SCANS
 # adapt moving of result
  
 # create PDF
@@ -100,23 +110,33 @@ echo 'creating PDF...'
 #pdftk *.tif.pdf cat output "$FILE_NAME.pdf"
 cp $WATCH_SCANS/$1/$FILE_NAME.pdf $OUT_DIR/
 exp_file="$OUT_DIR/$FILE_NAME.pdf"
+echo "output file $exp_file"
 
-# uncomment if you use rclone and adapt. -- Espacialy the email-adress.
-#rclone copy $exp_file OneDrive_Joe:2020/
-#onedrive_link=$(rclone link OneDrive_Joe:2020/$FILE_NAME.pdf)
+if [ -f $exp_file ]; then
+
+rclone copy $exp_file OneDrive_Joe:scanner/
+onedrive_link=$(rclone link OneDrive_Joe:scanner/$FILE_NAME.pdf)
+
 head_file=$( head -n 50 $WATCH_SCANS/$1/$FILE_NAME.txt )
 mail_text="
-Please find the file attached
- Original Path is
-$exp_file
-OneDrive_link
-$onedrive_link
+<html>
+<body>
+Please find the file attached<br>
+ Original Path is<br>
+$exp_file<p>
+OneDrive_link<br>
+<a href=\"$onedrive_link\">$onedrive_link</a><p>
 
-
------------------Content------------
+<h2>-----------------Content------------</h2>
 $head_file
+</body>
+</html>
 "
-echo $mail_text | mutt -s "$exp_file - ScanPi Mail" myemail@myemailprovider.com -a $exp_file
+
+echo $mail_text | mutt -e "set content_type=text/html" -s "$exp_file - ScanPi Mail" johannes@rumpf.name
+fi
+# attach file - max size problem
+# -a $exp_file
 
 #cd $WATCH_SCANS
 
@@ -125,11 +145,11 @@ if [ $TRASH_TMP_FILES -eq 1 ];
    then
 # delete steps
      rm -Rf $1
-     echo $1
+     echo "delete $1"
    else
 # save steps
-     mkdir $OUT_DIR/$FILE_NAME
-     mv $1 $OUT_DIR/$FILE_NAME/
+#     mkdir $OUT_DIR/$FILE_NAME
+#     mv $1 $OUT_DIR/$FILE_NAME/
      echo "move done"
    fi
 
